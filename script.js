84797b9fcf9196f8f67a41ed6576034ca78c929c
Jackson Nadler-Block <jnadlerblock@gmail.com>
2023-04-26 22:13:44 -0700

    Main Menu Setup

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ class LoadingScreen extends Phaser.Scene {
     }
 
     create() {
-        this.cameras.main.fadeIn(2000);
+        this.cameras.main.fadeIn(1000);
         this.add.image(300, 300, 'castle');
 
         //  Add graphics
@@ -40,9 +40,9 @@ class LoadingScreen extends Phaser.Scene {
         // let dot1 = this.graphics.fillRect(540, 570, 4, 4);
         // let dot2 = this.graphics.fillRect(555, 570, 4, 4);
         // let dot3 = this.graphics.fillRect(570, 570, 4, 4);
-        let dot1 = this.add.rectangle(540, 570, 4, 4, 0xffffff);
-        let dot2 = this.add.rectangle(555, 570, 4, 4, 0xffffff);
-        let dot3 = this.add.rectangle(570, 570, 4, 4, 0xffffff);
+        let dot1 = this.add.rectangle(540, 572, 4, 4, 0xffffff);
+        let dot2 = this.add.rectangle(555, 572, 4, 4, 0xffffff);
+        let dot3 = this.add.rectangle(570, 572, 4, 4, 0xffffff);
         
         let loading = this.add.text(400, 550, 'Loading');
         loading.setFontSize(30);
@@ -52,20 +52,20 @@ class LoadingScreen extends Phaser.Scene {
         // Make 3 dots bounce
         this.tweens.add({
             targets: dot1,
-            y: 564,
-            ease: "Sine",
+            y: 568,
+            ease: "Linear",
             duration: 700, 
-            repeat: -1,
+            repeat: 3,
             yoyo: true
         });
 
         this.time.delayedCall(500, () => {
             this.tweens.add({
                 targets: dot2,
-                y: 564,
-                ease: "Sine",
+                y: 568,
+                ease: "Linear",
                 duration: 700, 
-                repeat: -1,
+                repeat: 3,
                 yoyo: true
             });
         });
@@ -73,26 +73,65 @@ class LoadingScreen extends Phaser.Scene {
         this.time.delayedCall(1000, () => {
             this.tweens.add({
                 targets: dot3,
-                y: 564,
-                ease: "Sine",
+                y: 568,
+                ease: "Linear",
                 duration: 700, 
-                repeat: -1,
+                repeat: 3,
                 yoyo: true
             });
         });
-        /*this.tweens.add({
-            targets: loading,
-            y: 500,
-            ease: "Sine",
-            duration: 1000, repeat:-1,
-            yoyo:true
-        });*/
 
-        this.input.on('pointerdown', () => {
-            this.cameras.main.fadeOut(1000, 0,0,0);
-            this.time.delayedCall(1000, () => {
-                this.scene.start('game studio');
-            })
+        // Start next scene after dots bounce 3 times
+        this.time.delayedCall(4250, () => {
+            this.scene.start('main menu');
+        });
+
+        // this.input.on('pointerdown', () => {
+        //     this.cameras.main.fadeOut(1000, 0,0,0);
+        //     this.time.delayedCall(1000, () => {
+        //         this.scene.start('game studio');
+        //     })
+        // });
+    }
+}
+
+class MainMenu extends Phaser.Scene {
+    constructor() {
+        super('main menu');
+    }
+
+    preload() {
+        this.load.image('vampire', 'assets/Vampire.png');
+    }
+
+    create() {
+        this.cameras.main.fadeIn(1000);
+        let vampire = this.add.image(450, 300, 'vampire');
+
+        // this.input.on('pointerdown', () => {
+        //     this.cameras.main.fadeOut(1000, 0,0,0);
+        //     this.time.delayedCall(1000, () => {
+        //         this.scene.start('game studio');
+        //     })
+        // });
+
+        // Main Menu Text
+        // center align title and add animations to text
+        let title = this.add.text(60, 100, 'Vampire Adventures');
+        title.setFontSize(40);
+        title.setWordWrapWidth(200);
+        let newgame = this.add.text(60, 250, 'New Game');
+        newgame.setFontSize(30);
+        let loadgame = this.add.text(60, 325, 'Load Game');
+        loadgame.setFontSize(30);
+        let options = this.add.text(60, 400, 'Options');
+        options.setFontSize(30);
+        let exitgame = this.add.text(60, 475, 'Exit Game');
+        exitgame.setFontSize(30);
+
+        this.time.delayedCall(10000, () => {
+            this.cameras.main.fadeOut(500, 0,0,0);
+            this.scene.start('game studio');
         });
     }
 }
@@ -102,8 +141,7 @@ let config = {
     width: 600,
     height: 600,
     backgroundColor: 0x212121,
-    //scene: [GameStudio, LoadingScreen]
-    scene: [LoadingScreen]
+    scene: [GameStudio, LoadingScreen, MainMenu]
 }
 
 let game = new Phaser.Game(config);
